10a3ecb1ffeae6f00162259d4de13f3b3d09d36e
Izaz <izazul2016s@gmail.com>
2022-05-23 02:35:46 +0600

    Card

--- lib/Design.dart
+++ lib/Design.dart
@@ -18,95 +18,208 @@ class MyWidget extends StatelessWidget {
       appBar: AppBar(title: Text("My Home Page")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => MyList()));
-                      },
-                      child: Card(
-                        color: Colors.green[200],
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(50),
-                          ),
-                        ),
-                        child: Container(
-                          height: 80,
-                          width: 500,
-                          child: Center(
-                              child: Text(
-                            "Bangla Kobita",
-                            style: TextStyle(fontSize: 30),
-                          )),
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => WeatherSide()));
-                      },
-                      child: Card(
-                        color: Colors.green[200],
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(50),
-                          ),
-                        ),
-                        child: Container(
-                          height: 80,
-                          width: 500,
-                          child: Center(
-                            child: Text(
-                              "Weather Side",
-                              style: TextStyle(fontSize: 30),
+        child: SingleChildScrollView(
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("My Home Page",style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+              ),),
+              SizedBox(height: 10,),
+              Stack(
+                children: [
+
+                  Column(
+                    children: [
+
+
+                      SingleChildScrollView(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => MyList()));
+                              },
+                              child: Card(
+                                color: Colors.purple,
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                      child: Text(
+                                        "Bangla Kobita",
+                                        style: TextStyle(fontSize: 30),
+                                      )),
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PoemSide()));
-                      },
-                      child: Card(
-                        color: Colors.green[200],
-                        elevation: 2,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(50),
-                          ),
-                        ),
-                        child: Container(
-                          height: 80,
-                          width: 500,
-                          child: Center(
-                            child: Text(
-                              "ListView Side",
-                              style: TextStyle(fontSize: 30),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => WeatherSide()));
+                              },
+                              child: Card(
+                                color: Colors.purple[200],
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                    child: Text(
+                                      "Weather Side",
+                                      style: TextStyle(fontSize: 30),
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ),
-                          ),
+
+                          ],
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
-            )
-          ],
+                      SingleChildScrollView(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => MyList()));
+                              },
+                              child: Card(
+                                color: Colors.deepOrange,
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                      child: Text(
+                                        "Bangla Kobita",
+                                        style: TextStyle(fontSize: 30),
+                                      )),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => WeatherSide()));
+                              },
+                              child: Card(
+                                color: Colors.pink,
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                    child: Text(
+                                      "Weather Side",
+                                      style: TextStyle(fontSize: 30),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      SingleChildScrollView(
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => PoemSide()));
+                              },
+                              child: Card(
+                                color: Colors.green[200],
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                    child: Text(
+                                      "ListView Side",
+                                      style: TextStyle(fontSize: 30),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => PoemSide()));
+                              },
+                              child: Card(
+                                color: Colors.pinkAccent[200],
+                                elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(30),
+                                  ),
+                                ),
+                                child: Container(
+                                  width: 150,
+                                  height: 200,
+                                  child: Center(
+                                    child: Text(
+                                      "ListView Side",
+                                      style: TextStyle(fontSize: 25),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              )
+            ],
+          ),
         ),
       ),
     ));
